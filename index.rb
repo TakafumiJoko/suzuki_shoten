abf8acdd0daa1fb28bbeb0cc33ff149e15f6d79f
joko-takafumi <jktkfm5535@gmail.com>
2023-11-14 23:44:26 +0900

    Implement Check Answer Method

--- index.rb
+++ index.rb
@@ -1,22 +1,35 @@
+$answers = [];
+$words = [];
+
 def input
   size = gets.to_i
   p size
   i = 0
-  answers = []
   while i < size do
-    answers << gets.split(' ')
+    $answers << gets.split(' ')
     i += 1
   end
-  p answers
+  p $answers
   num = gets.to_i
   p num
   i = 0
-  words = []
   while i < num do 
-    words << gets
+    $words << gets.chomp
     i += 1
   end
-  p words
+  p $words
+end
+
+def check_answer
+  $answers.each_with_index do |row, row_i|
+    row.each_with_index do |answer, answer_i|
+      if $words.include?($answers[row_i][answer_i])
+        $answers[row_i][answer_i] = nil
+      end
+    end
+  end
+  p $answers
 end
 
 input
+check_answer
