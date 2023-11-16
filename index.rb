fe85af56e3067aae4bd25f37d620725b637e31ca
joko-takafumi <jktkfm5535@gmail.com>
2023-11-16 17:47:42 +0900

    Add Comment

--- index.rb
+++ index.rb
@@ -21,6 +21,7 @@ end
 def check_answer
   $answers.each_with_index do |row, row_i|
     row.each_with_index do |col, col_i|
+      # 一致した単語（$words）の中にマスの単語（$answers[row_i][col_i]）が含まれていたら、そのマスをチェックする。
       if $words.include?($answers[row_i][col_i])
         $answers[row_i][col_i] = nil
       end
@@ -31,6 +32,7 @@ end
 def judge_horizontal
   $answers.each_with_index do |row, row_i|
     row.each_with_index do |col, col_i|
+      # １行内で最後のマスまでnilの値が連続すれば、true（ビンゴの意）を返す
       if col.nil?
         if col_i == $size - 1
           return true
@@ -47,6 +49,7 @@ end
 def judege_vertical
   for col in 0..$size - 1 do
     for row in 0..$size - 1 do
+      # １列内で最後のマスまでnilの値が連続すれば、true（ビンゴの意）を返す
       if $answers[col][row].nil?
         if row == $size - 1
           return true
@@ -62,7 +65,7 @@ end
 
 def judge_diagonal
   i = 0
-
+   # 斜めの並び（左上隅から右下隅まで）の最後のマスまでnilの値が連続すれば、true（ビンゴの意）を返す
    for i in 0..$size - 1 do
     if $answers[i][i].nil?
       if i == $size - 1
@@ -72,7 +75,7 @@ def judge_diagonal
       break
     end
   end
-
+   # 斜めの並び（右上隅から左下隅まで）の最後のマスまでnilの値が連続すれば、true（ビンゴの意）を返す
    for i in 0..$size - 1 do
     if $answers[i][$size - 1 - i].nil?
       if i == $size - 1
