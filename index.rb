eaf0790f8f3981b79a8c1f8e0a443f90d6dfbac7
joko-takafumi <jktkfm5535@gmail.com>
2023-11-16 12:32:04 +0900

    Implement Judge Result Method

--- index.rb
+++ index.rb
@@ -1,35 +1,104 @@
-$answers = [];
-$words = [];
+$size = 0
+$answers = []
+$words = []
+$result
 
 def input
-  size = gets.to_i
-  p size
+  $size = gets.to_i
   i = 0
-  while i < size do
+  while i < $size do
     $answers << gets.split(' ')
     i += 1
   end
-  p $answers
   num = gets.to_i
-  p num
   i = 0
   while i < num do 
     $words << gets.chomp
     i += 1
   end
-  p $words
 end
 
 def check_answer
   $answers.each_with_index do |row, row_i|
-    row.each_with_index do |answer, answer_i|
-      if $words.include?($answers[row_i][answer_i])
-        $answers[row_i][answer_i] = nil
+    row.each_with_index do |col, col_i|
+      if $words.include?($answers[row_i][col_i])
+        $answers[row_i][col_i] = nil
       end
     end
   end
-  p $answers
+end
+
+def judge_horizontal
+  $answers.each_with_index do |row, row_i|
+    row.each_with_index do |col, col_i|
+      if col.nil?
+        if col_i == $size - 1
+          p 'yes:38'
+          return true
+        end
+      else
+        break
+      end
+    end
+  end
+
+  return false
+end
+
+def judege_vertical
+  for col in 0..$size - 1 do
+    for row in 0..$size - 1 do
+      if $answers[col][row].nil?
+        if row == $size - 1
+          p 'yes:55'
+          return true
+        end
+      else
+        break
+      end
+    end
+  end
+
+  return false
+end
+
+def judge_diagonal
+  i = 0
+
+   for i in 0..$size - 1 do
+    if $answers[i][i].nil?
+      if i == $size - 1
+        p 'yes:73'
+        return true
+      end
+    else
+      break
+    end
+  end
+
+   for i in 0..$size - 1 do
+    if $answers[i][$size - 1 - i].nil?
+      if i == $size - 1
+        p 'yes:84'
+        return true
+      end
+    else
+      break
+    end
+  end
+
+  return false
+end
+
+def judge_result
+  if judge_horizontal || judege_vertical || judge_diagonal
+    $result = 'yes'
+  else
+    $result = 'no'
+  end
 end
 
 input
 check_answer
+judge_result
+p $result
